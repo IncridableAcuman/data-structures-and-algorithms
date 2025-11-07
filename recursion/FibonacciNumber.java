319ce517d8281b69893f792480880603582fdce5
Izzatbek <abdusharipovizzat03@gmail.com>
2025-11-07 10:06:29 +0500

    data structures and algorithms

---
+++ recursion/FibonacciNumber.java
@@ -0,0 +1,13 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n=scanner.nextInt();
+            System.out.println(n);
+        }
+    }
+}
---
+++ sorting/BubbleSort.java
@@ -0,0 +1,11 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println(n);
+    }
+}
