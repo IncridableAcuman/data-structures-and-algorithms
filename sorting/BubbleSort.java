9bc1acdcf2a2cf883c2547452b6df0ee4b894c20
Izzatbek <abdusharipovizzat03@gmail.com>
2025-11-07 10:12:22 +0500

    data structures and algorithms

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -3,9 +3,28 @@ package sorting;
 import java.util.Scanner;
 
 public class BubbleSort {
+    public static void bubbleSortAlgorithm(int[] ar){
+        int n=ar.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(ar[j]>ar[j+1]){
+                    int t=ar[j];
+                    ar[j]=ar[j+1];
+                    ar[j+1]=t;
+                }
+            }
+        }
+        for (int j : ar) {
+            System.out.print(j + " ");
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n=scanner.nextInt();
-        System.out.println(n);
+        int[] ar=new int[n];
+        for (int i=0;i<n;i++){
+            ar[i] = scanner.nextInt();
+        }
+        bubbleSortAlgorithm(ar);
     }
 }
